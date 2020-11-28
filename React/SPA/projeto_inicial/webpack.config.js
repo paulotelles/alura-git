191e4e74be1bb72688c64039242479ff4e85759e
PauloMarxs <paulomarquestelles@gmail.com>
2020-11-28 14:23:42 -0300

    alterações 28/11
    alterações

--- React/SPA/projeto_inicial/webpack.config.js
+++ React/SPA/projeto_inicial/webpack.config.js
@@ -1,10 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
     mode: "development",
+    devServer: {
+        open: true,
+        contentBase: "dist"
+    },
     entry: "./src/componentes/lista/listagem-cliente.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/clientes.html",
+        filename: "index.html"
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: "[name].[ext]"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            }
+        ]
     }
 }
