c4183be400897002733343d47ebc2a664315bda8
PauloMarxs <paulomarquestelles@gmail.com>
2020-12-08 17:17:26 -0300

    criacao do app react

---
+++ React/React-Aula1/ceep/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <form>
+      <input type="text" placeholder="Titulo"/>
+      <textarea placeholder="Digite aqui sua nota..."/>
+
+    </form>
+  );
+}
+
+export default App;
